3443015c51e9383970c1afbd8658dd79f19d83cb
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-07-31 16:53:14 -0300

    Homework 1 - 01

--- FT-M1/01-IntroToCS/homework/homework.js
+++ FT-M1/01-IntroToCS/homework/homework.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   return parseInt(num, 2)
+   let numeros = 0;
+   let numArray = num.split("").reverse();
+
+   for (let i = 0; i < num.length; i++) {
+      numeros = numeros + numArray[i] * 2 ** i;
+   } return numeros;
 }
 
 function DecimalABinario(num) {
