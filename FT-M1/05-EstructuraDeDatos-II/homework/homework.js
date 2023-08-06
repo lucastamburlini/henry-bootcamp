864865316324d1fe44a5b999b767f58aa9e01b7d
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-05 23:32:24 -0300

    hw-05

--- FT-M1/05-EstructuraDeDatos-II/homework/homework.js
+++ FT-M1/05-EstructuraDeDatos-II/homework/homework.js
@@ -116,7 +116,28 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() { }
+class HashTable {
+  constructor() {
+    this.numBuckets = 35;
+    this.buckets = new Array(this.numBuckets);
+  }
+
+  hash() {
+
+  }
+
+  set() {
+
+  }
+
+  get() {
+
+  }
+
+  hasKey() {
+
+  }
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
