f1ed208debb43da82cb63b38ef6a55fafac72052
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-09 13:01:53 -0300

    hw-07

--- FT-M1/08-Algoritmos-II/homework/homework.js
+++ FT-M1/08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,12 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  // [7,2,8,5,3,9,4]
+  // Condición de corte = if([1]) => array
+  // let pivote = Math.random()
+  // recorrer el array i < pivote = izq[] && i > pivote = der[]
+  // quickSort(izq)  quickSort(der)
+  // return array
 }
 
 function mergeSort(array) {
@@ -16,6 +22,25 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+
+
+  // Podemos trabajar con tres funciones independientes.
+  // [7,2,8,5,3,9,4]
+  // if([1]) = return array
+  // dividir([])
+  // izq[] | der[]
+  // merge(margeSort(izq[]) + margeSort.(der[])) solo vamos a unir los array que tiene un solo elemento
+}
+
+
+function dividir(){
+ // dividi a la mitad
+ // devuelve la izquierda
+ // devuelve la derecha
+}
+function marge(izq, der){
+  // izq[] < o > der[]
+  // [] oredenado
 }
 
 // No modificar nada debajo de esta línea
