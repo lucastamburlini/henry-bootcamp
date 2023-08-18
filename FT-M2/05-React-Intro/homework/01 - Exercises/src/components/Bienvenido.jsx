7ada0f32d58327e098ce9d7a197e6241022ef4cc
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-18 16:48:20 -0300

    hw-05

--- FT-M2/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2/05-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Lucas Tamburlini";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+
+  return (
+    <div>
+      <h1>Soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((tech) => (
+          <li>{tech}</li>
+        ))}
+      </ul>
+      <Botones />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
---
+++ FT-M2/05-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default Botones = () => {
+  return (
+    <div>
+      <button onClick={() => alert("Hola!")}>Módulo 1</button>
+      <button onClick={() => alert("Hola 2!")}>Módulo 2</button>
+    </div>
+  );
+};
