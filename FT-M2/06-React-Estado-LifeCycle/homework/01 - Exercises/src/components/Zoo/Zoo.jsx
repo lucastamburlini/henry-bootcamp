238e81b9e99663c45a30c44ba3507d1077e9eaca
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-22 15:04:31 -0300

    hw-06

--- FT-M2/06-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ FT-M2/06-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // eslint-disable-next-line no-unused-vars
 import Animals from "../Animals/Animals";
 // eslint-disable-next-line no-unused-vars
@@ -7,11 +7,40 @@ import Species from "../Species/Species";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  const [zoo, setZoo] = React.useState({
+    zooName: "",
+    animals: [],
+    species: [],
+    allAnimals: [],
+  });
+
+  const handleInputChange = (evento) => {
+    setZoo({
+      ...zoo,
+      zooName: evento.target.value,
+    });
+  };
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/zoo")
+      .then((res) => res.json())
+      .then((data) =>
+        setZoo({
+          ...zoo,
+          animals: data.animals,
+          species: data.species,
+          allAnimals: data.animals,
+        })
+      )
+      .catch((error) => console.log(error));
+  }, []);
 
 
   return (
     <div>
-      <h1>-</h1>
+      <label>Zoo Name:</label>
+      <input value={zoo.zooName} onChange={handleInputChange} />
+      <h1>{zoo.zooName}</h1>
     </div>
   );
 }
