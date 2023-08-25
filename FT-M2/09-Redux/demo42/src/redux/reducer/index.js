c326f5a326310db313e708793040f174244dbcb3
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-25 16:20:29 -0300

    hw-09

---
+++ FT-M2/09-Redux/demo42/src/redux/reducer/index.js
@@ -0,0 +1,26 @@
+// const initialState = {contador: 0, nombre: "Gamaliel", coleguillas: []};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case AUMENTAR_CONTADOR:
+//       return {
+//         ...state,
+//         contador: state.contador + 1,
+//       };
+
+//     case DISMINUIR_CONTADOR:
+//       return {
+//         ...state,
+//         contador: state.contador - 1,
+//       };
+
+//     case AGREGAR:
+//       return {
+//         ...state,
+//         coleguillas: [...state.coleguillas, action.payload],
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
---
+++ FT-M2/09-Redux/demo42/src/redux/store/index.js
@@ -0,0 +1,4 @@
+// import {createStore} from "redux";
+// import {thunk} from "thunk" ->middleware es una funcion intermediaria
+
+// const store = createStore(reducer);
