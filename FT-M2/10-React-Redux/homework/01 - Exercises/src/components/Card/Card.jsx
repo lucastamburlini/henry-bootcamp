459e11a199f6761fe7f2f944a5d321f490d9869e
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-28 20:04:35 -0300

    hw-10

--- FT-M2/10-React-Redux/homework/01 - Exercises/src/components/Card/Card.jsx
+++ FT-M2/10-React-Redux/homework/01 - Exercises/src/components/Card/Card.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './card.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./card.css";
+import { deleteProduct } from "../../redux/actions/actions";
 
-export class Card extends React.Component{
-   constructor(props){
-      super(props)
-   }
+export class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-   render(){
-      return (
-         <div className='cardBg'>
-            <h5>{this.props.name}: </h5>
-            <h5>${this.props.price}</h5>
-            <button className='cardBtn'>X</button>
-         </div>
-      )
-   };
-};
+  handleDelete = () => {
+    this.props.deleteProduct(this.props.id);
+  };
 
-export function mapDispatchToProps() {}
+  render() {
+    return (
+      <div className="cardBg">
+        <h5>{this.props.name}: </h5>
+        <h5>${this.props.price}</h5>
+        <button onClick={this.handleDelete} className="cardBtn">
+          X
+        </button>
+      </div>
+    );
+  }
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    deleteProduct: (id) => {
+      dispatch(deleteProduct(id));
+    },
+  };
+}
 
 export default connect(null, mapDispatchToProps)(Card);
