da885a8f30154b221bc777033d558707247c3a1f
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-04 20:10:54 -0300

    hw-m3-01

--- FT-M3/01-Node/homework/01 - Exercises/commands/index.js
+++ FT-M3/01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,62 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(".", (error, files) => {
+        if (error) throw error
+        print(files.join(" "))
+    })
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error
+        print(data)
+    })
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error;
+        const firstLines = data.split("\n")[0].trim()
+        print(firstLines)
+    })
+}
 
-function tail() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error;
+        const lastLine = data.split("\n")
+        print(lastLine.at(-1).trim())
+    })
+}
 
-function curl() {}
+function curl(print, args) {
+    utils.request(args, (error, response) => {
+        if (error) return error
+        print(response)
+    })
+}
 
-module.exports = {};
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
+};
