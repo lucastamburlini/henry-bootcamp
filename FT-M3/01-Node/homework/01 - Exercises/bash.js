4711178a34851e253f13df885eb384b4b4f09d9a
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-04 20:11:11 -0300

    hw-01

--- FT-M3/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3/01-Node/homework/01 - Exercises/bash.js
@@ -2,8 +2,20 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
+function print(output) {
+   process.stdout.write(output)
+   process.stdout.write("\nprompt > ")
+}
+
 function bash() {
-   "hola"
+   process.stdout.write("prompt > ");
+   process.stdin.on("data", (data) => {
+      const args = data.toString().trim().split(" ")
+      const cmd = args.shift()
+
+      commands[cmd] ? commands[cmd](print, args.join(" ")) : print(`command not found: ${cmd}`)
+   })
+
 }
 
 bash();
