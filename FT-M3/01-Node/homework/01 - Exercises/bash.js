dd108337def7ac6391b238057f41f01dde2ebf7b
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-04 12:50:52 -0300

    add module 3

---
+++ FT-M3/01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,13 @@
+const process = require('process');
+const { Z_ASCII } = require('zlib');
+const commands = require('./commands/index.js');
+
+function bash() {
+   "hola"
+}
+
+bash();
+module.exports = {
+   print,
+   bash,
+};
