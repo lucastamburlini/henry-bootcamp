7a9c4fa9d9aaf5bd8edb6dfe84077349b39e7f05
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-20 20:11:27 -0300

    he-03

--- FT-M4/03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
+++ FT-M4/03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
@@ -7,19 +7,23 @@ module.exports = (sequelize) => {
          primaryKey: true,
       },
       name: {
-
+         type: DataTypes.STRING,
+         unique: true
       },
       age: {
-
+         type: DataType.INTEGER,
       },
       race: {
-
+         type: DataType.ENUM('Human', 'Elf', 'Machine', 'Demon', 'Animal', 'Other'),
+         defaultValue: "Other"
       },
       hp: {
-
+         type: DataType.FLOAT,
+         allowNull: false
       },
       mana: {
-
+         type: DataType.FLOAT,
+         allowNull: false
       },
    },
       { timestamps: false });
